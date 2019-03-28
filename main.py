5738d0433a7223d784f4fb537c1ff8b06fc2725a
Jelle Hulter <jellehulter@gmail.com>
2019-03-28 15:51:45 +0100

    Moved img file to argument instead of input

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import imageio
 import numpy as np
+import sys
 
-filename = str(input())
+filename = str(sys.argv[1])
 im = imageio.imread(filename)
 
 width = im.shape[0]
 height = im.shape[1]
+
+maze = np.array([])
+
+print(maze)
