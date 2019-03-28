4379a79ee5b3876d14726239e13f873406f85e33
Jelle Hulter <jellehulter@gmail.com>
2019-03-28 16:21:14 +0100

    Added pixel values to maze array

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ im = imageio.imread(filename)
 width = im.shape[0]
 height = im.shape[1]
 
-maze = np.array([])
+maze = np.zeros((width, height) , dtype=int)
+
+for x in range(width):
+    for y in range(height):
+        if(im[y][x][0] == 255):
+            maze[y][x] = 1
 
 print(maze)
